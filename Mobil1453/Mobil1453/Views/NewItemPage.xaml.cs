92222177a1f863a0138cfdd53e4c3b2d734c4a08
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-12-08 17:39:47 +0300

    mobile

---
+++ Mobil1453/Mobil1453/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Mobil1453.Models;
+using Mobil1453.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobil1453.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
