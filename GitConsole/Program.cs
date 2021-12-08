96bf57232ea64ba08ecbcbb520fa81beac15d84f
gkrctn <gkr.cetin@gmail.com>
2021-12-08 17:16:10 +0300

    göker

--- GitConsole/Program.cs
+++ GitConsole/Program.cs
@@ -7,8 +7,9 @@ namespace GitConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Wm1");
-            Console.WriteLine("Mesut");
+            Console.WriteLine("Mesut Öztürk");
             Console.WriteLine("Emre");
+            Console.WriteLine("Göker");
         }
     }
 }
