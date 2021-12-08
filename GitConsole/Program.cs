bf54626147dd10ced0d7a6722f52710a8fcb13f3
resulyag <resulyag@gmail.com>
2021-12-08 17:07:57 +0300

    Resul

--- GitConsole/Program.cs
+++ GitConsole/Program.cs
@@ -9,6 +9,7 @@ namespace GitConsole
             Console.WriteLine("Hello Wm1");
             Console.WriteLine("Mesut");
             Console.WriteLine("Emre");
+            Console.WriteLine("Resul");
         }
     }
 }
