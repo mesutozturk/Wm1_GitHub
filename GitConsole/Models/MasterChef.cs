6fcd31bd5dd832a74ab404a476603f136b5e870c
osmntrk <osmanturk41@gmail.com>
2021-12-08 17:25:08 +0300

    osman

---
+++ GitConsole/Models/MasterChef.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitConsole.Models
+{
+    public class MasterChef
+    {
+        public int Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+}
