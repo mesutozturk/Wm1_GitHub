2bb538daa6534c4b0d37f32d841fdef748ba5858
resulyag <resulyag@gmail.com>
2021-12-08 17:32:53 +0300

    resulyg

--- GitConsole/Program.cs
+++ GitConsole/Program.cs
@@ -31,7 +31,6 @@ namespace GitConsole
             Console.WriteLine("Göker");
             Console.WriteLine("Ronaldo");
             Console.WriteLine("Göker");
-            Console.WriteLine("İrem");
         }
     }
 }
