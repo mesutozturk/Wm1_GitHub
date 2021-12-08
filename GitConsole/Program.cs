7a5c95dc00ef9ec07272933e50abf0a9a7aa9911
resulyag <resulyag@gmail.com>
2021-12-08 17:17:45 +0300

    Resul

--- GitConsole/Program.cs
+++ GitConsole/Program.cs
@@ -9,8 +9,8 @@ namespace GitConsole
             Console.WriteLine("Hello Wm1");
             Console.WriteLine("Mesut Öztürk");
             Console.WriteLine("Emre");
-            Console.WriteLine("Mert Vurgun");
             Console.WriteLine("Resul");
+            Console.WriteLine("Mert Vurgun");
             Console.WriteLine("Messi");
             Console.WriteLine("Yunus");
             Console.WriteLine("Ahmet");
