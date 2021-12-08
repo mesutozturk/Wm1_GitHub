1a438017df83bc6cbdb663c9883384f4a19595a6
onurysr <onurysr97@gmail.com>
2021-12-08 17:19:08 +0300

    cr7

--- GitConsole/Program.cs
+++ GitConsole/Program.cs
@@ -20,6 +20,7 @@ namespace GitConsole
             Console.WriteLine("Sait");
             Console.WriteLine("Eda");
             Console.WriteLine("Onur");
+            Console.WriteLine("Ronaldo");
         }
     }
 }
